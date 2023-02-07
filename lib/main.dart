4fe225f38432cda88d79029081842f802161bcfd
pasuta-jp <akama0519@outlook.jp>
2023-02-07 17:24:16 +0900

    add multi lang mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:developer';
 import './view/AddMemo.dart';
 import './view/List.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: L10n.localizationsDelegates, // これ！
+      supportedLocales: L10n.supportedLocales, // これ！
       title: 'My Todo App',
       theme: ThemeData(
         primarySwatch: Colors.purple,
